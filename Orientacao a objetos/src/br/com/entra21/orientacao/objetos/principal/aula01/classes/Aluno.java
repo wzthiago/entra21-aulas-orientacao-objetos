b2279d76ea58f5ae76c54cde009d77886eb74fa3
wzthiago <wzthiago@gmail.com>
2022-05-20 21:18:53 -0300

    Atualizando no eclipse...

--- Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/aula01/classes/Aluno.java
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/aula01/classes/Aluno.java
@@ -3,31 +3,28 @@ package br.com.entra21.orientacao.objetos.principal.aula01.classes;
 import java.util.Scanner;
 
 public class Aluno {
-	
-	// definir atributos de um aluno e cada aluno tera essas variaveis, chamadas de atributos
-	
+
+	// definir atributos de uma aluno e cada objeto aluno tera essas variaveis,
+	// chamadas de atributos
 	public String nome;
 	public byte idade;
 	public byte quantidadePresencas;
-	
-	// na declaração só tem, acesso e tipo de retorno
-	public Aluno() { // construtor vazio que ao utilizar o (new Aluno) ele é executado
-		
+
+	// na declaração so tem , acesso e tipo de retorno
+	public Aluno() {// construtor vazio que ao utilizar o (new Aluno) ele é executado
+
 	}
-	
-	
-	public byte responderChamada () {
-		
+
+	public byte responderChamada() {
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("O aluno "+this.nome);
+		System.out.println("O aluno " + this.nome + "está ai?");
 		String resposta = entrada.next();
-		if(resposta.equalsIgnoreCase("sim")) {
+		if (resposta.equalsIgnoreCase("sim")) {
 			this.quantidadePresencas++;
-			//o this serve para reafirmar que 
-		}	//um atributo do meu objeto
+			// o this serve para reafirmar que estamos mencionando
+			// um atributo do meu objeto
+		}
 		return this.quantidadePresencas;
 	}
-	
-	
-	
+
 }
